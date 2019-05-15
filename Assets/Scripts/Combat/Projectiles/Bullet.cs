490c5104bb983934247b59e868f1064eb8901e96
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-05-15 16:02:28 +1000

    Gun and sword

---
+++ Assets/Scripts/Combat/Projectiles/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Projectile
+{
+    public float speed = 10f;
+    public Transform line;
+    public GameObject effectPrefab;
+
+    private Rigidbody rigid;
+   
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Check if velocity is not zero
+        if(rigid.velocity.magnitude > 0f)
+        {
+            // Rotate using LookRotation
+            line.rotation = Quaternion.LookRotation(rigid.velocity);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // If there is an attached effect
+        if(effectPrefab)
+        {
+            // Get the contact point
+            ContactPoint contact = col.contacts[0];
+            // Spawn the effect - and rotate to contact normal
+            Instantiate(effectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+        }
+        //Destroy self
+        Destroy(gameObject);
+    }
+
+    public override void Fire(Vector3 lineOrigin, Vector3 direction)
+    {
+        rigid.AddForce(direction * speed, ForceMode.Impulse);
+        line.position = lineOrigin;
+    }
+}
---
+++ Assets/Scripts/Combat/Weapons/Gun.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Weapon
+{
+    public int maxReserve = 300, maxClip = 30;
+    public float spread = 2f, recoil = 1f;
+    public Transform shotOrigin;
+    public GameObject projectilePrefab;
+
+    private int currentReserve = 0, currentClip = 0;
+    void Start()
+    {
+        Reload();
+    }
+
+    public void Reload()
+    {
+        // If there is ammon in reserve
+        if(currentReserve > 0)
+        {
+            // If reserve is greater than max clip
+            if(currentReserve >= maxClip)
+            {
+                // Remove difference from current reserve
+                int difference = maxClip - currentClip;
+                currentReserve -= difference;
+                // Replenish entire clip with max clip
+                currentClip = maxClip;
+            }
+            // If clip < max clip
+            if(currentClip < maxClip)
+            {
+                // Set entire clip to reserve
+                currentClip += currentReserve;
+                currentReserve -= currentReserve;
+            }
+        }
+    }
+
+    public override void Attack()
+    {
+        // Attack logic
+        // Reduce the clip
+        currentClip--; // currentClip = currentClip - 1 / currentClip -= 1
+        // Get Origin + Direction for Bullet
+        Camera attachedCamera = Camera.main;
+        Transform camTransform = attachedCamera.transform;
+        Vector3 lineOrigin = shotOrigin.position;
+        Vector3 direction = camTransform.forward;
+        GameObject clone = Instantiate(projectilePrefab, camTransform.position, camTransform.rotation);
+        Projectile projectile = clone.GetComponent<Projectile>();
+        projectile.Fire(lineOrigin, direction);
+
+        // Reset ability to attack
+        base.Attack();
+    }
+}
